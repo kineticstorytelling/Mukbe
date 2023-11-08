36225ba05e6227c74eefb66ef309f43ccb278533
kineticstorytelling <kineticstorytelling@gmail.com>
2023-11-07 23:24:43 -0500

    converted back to js for now

---
+++ components/welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { View } from "react-native"
+
+
+const Welcome = () => {
+  return (
+    <View>
+        <Text>
+          View   
+        </Text>
+    </View>
+  )
+}
+
+export default Welcome
