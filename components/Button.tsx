c30b6e62d7ac5bf5a07a5e1efdb0a88cc46d94fa
kineticstorytelling <kineticstorytelling@gmail.com>
2023-11-17 16:09:40 -0500

    new button file

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface ButtonProps {
+    // children:React.ReactNode;
+    className?: string;
+}
+
+const Button = (props: Props) => {
+  return (
+    <div>Button</div>
+  )
+}
